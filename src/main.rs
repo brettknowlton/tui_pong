58afe7cf3310b88a3527ceab8b348b33b85cb5a1
brettknowlton <brettlknowlton@gmail.com>
2024-05-19 18:36:17 -0600

    Ball physics is close need to define borders a little better

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ratatui::{
     symbols::border,
     widgets::{block::*, *},
 };
-use std::{io, time::Duration, vec};
+use std::{cmp::{max, min}, io, time::Duration, vec};
 
 mod tui;
 #[derive(Debug, Default)]
@@ -35,7 +35,7 @@ impl App {
             self.update()?;
             
             self.handle_events()?;
-             
+            
             terminal.draw(|frame| self.render_frame(frame))?;
         }
         Ok(())
@@ -62,7 +62,7 @@ impl App {
 
     fn handle_events(&mut self) -> io::Result<()> {
 
-        if event::poll(Duration::from_millis(32))? {
+        if event::poll(Duration::from_millis(82))? {
             // It's guaranteed that `read` won't block, because `poll` returned
             // `Ok(true)`.
             match event::read()? {
@@ -101,47 +101,39 @@ impl App {
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let layout = Layout::default()
+        let layoutH = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints([Constraint::Length(90), Constraint::Min(10)].as_ref())
+            .constraints([Constraint::Length(100), Constraint::Min(10)].as_ref())
             .split(area);
 
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
+        let layoutV = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(30), Constraint::Min(10)].as_ref())
+            .split(layoutH[0]);
+
+        let title = Title::from(" Stats : ".bold());
         let block = Block::default()
             .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
             .borders(Borders::ALL)
             .border_set(border::THICK)
-            .red();
+            .white();
 
         let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
+            "Ball_x: ".into(),
+            self.game.ball.0.to_string().yellow(),
         ])]);
 
         Paragraph::new(counter_text)
             .centered()
             .block(block)
-            .render(layout[1], buf);
+            .render(layoutH[1], buf);
 
         match self.state {
             State::Menu => {
                 //self.render_menu(layout[0], buf);
             }
             State::Game => {
-                self.game.render(layout[0], buf);
+                self.game.render(layoutV[0], buf);
             }
             State::GameOver => {
                 //self.render_game_over(layout[0], buf);
@@ -154,6 +146,7 @@ impl Widget for &App {
 
 #[derive(Debug)]
 struct Game{
+    score: (u8, u8),
     //ball position + velocity
     ball: (f32, f32),
     ball_v: (f32, f32),
@@ -170,15 +163,67 @@ struct Game{
 impl Game{
     fn update(&mut self) {
 
-        if(self.ball.0 + self.ball_v.0) > 1.0 || (self.ball.0 + self.ball_v.0) < 0.0 {
+        if(self.ball.0 + self.ball_v.0) >= 1.0 || (self.ball.0 + self.ball_v.0) < 0.0 {
             self.ball_v.0 = -self.ball_v.0;
         }
         self.ball.0 += self.ball_v.0;
 
-        if(self.ball.1 + self.ball_v.1) > 1.0 || (self.ball.1 + self.ball_v.1) < 0.0 {
+        if(self.ball.1 + self.ball_v.1) >= 1.0 || (self.ball.1 + self.ball_v.1) <= 0.0 {
             self.ball_v.1 = -self.ball_v.1;
         }
         self.ball.1 += self.ball_v.1;
+    }
+
+    fn draw_ball(&self, area: Rect, buf: &mut Buffer) {
+        let ball_text_styles: Vec<Vec<(&str, &str)>> = vec!(
+            vec!(("▗▄", "▝▀"), ("▗▄▖", "▝▀▘"), ("▄▖", "▀▘")),
+            vec!(("▐█▌", ""), ("██", ""), ("▐█▌", "")),
+            vec!(("▗▄", "▝▀"), ("▗▄▖", "▝▀▘"), ("▄▖", "▀▘")),
+        );
+            
+        let denormalized_x = self.ball.0 * area.width as f32;
+        let mut x_coord: i16;
+
+        if denormalized_x - denormalized_x.floor() > 0.33 && denormalized_x - denormalized_x.floor() < 0.66{
+            x_coord = 0;
+        }else if denormalized_x - denormalized_x.floor() > 0.66{
+            x_coord = 1;
+        }else{
+            x_coord = -1;
+        }
+
+
+        let denormalized_y = self.ball.1 * area.height as f32;
+        let mut y_coord: i16;
+
+        if denormalized_y - denormalized_y.floor() > 0.33 && denormalized_y - denormalized_y.floor() < 0.66{
+            y_coord = 1;
+        }else if denormalized_y - denormalized_y.floor() > 0.66{
+            y_coord = 2;
+        }else{
+            y_coord = 0;
+        }
+
+        let ball_text = ball_text_styles[y_coord as usize][(x_coord + 1) as usize];
+
+        y_coord = min(y_coord, 1);
+
+        let line1 = ball_text.0;
+        let line1_x = max((self.ball.0 * area.width as f32) as i16 + (x_coord - 1) as i16 - 2, 0);
+        let line1_y = (self.ball.1 * area.height as f32) as u16 + max(y_coord - 1 , 0) as u16;
+        buf.set_string(line1_x as u16, line1_y, line1, Color::Yellow);
+
+        let line2 = ball_text.1;
+        let line2_x = (self.ball.0 * area.width as f32) as u16 + max(x_coord - 1 , 0) as u16 - 2;
+        let line2_y = min(
+            (self.ball.1 * area.height as f32) as u16 + 
+                max(
+                    y_coord - 1 , 
+                    0
+                ) as u16 + 1, 
+            area.height - 1 
+        ) as u16;
+        buf.set_string(line2_x, line2_y, line2, Color::Yellow);
     }
 
 }
@@ -186,8 +231,9 @@ impl Game{
 impl Default for Game {
     fn default() -> Self {
         Self {
+            score: (0, 0),
             ball: (0.0, 0.0),
-            ball_v: (0.015, 0.024),
+            ball_v: (0.055, 0.0275),
             p1: (0.0, 0.0),
             p1_v: 0.0,
             p2: (0.0, 0.0),
@@ -198,40 +244,30 @@ impl Default for Game {
 
 impl Widget for &Game {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        //draw ball
+        
 
-        let ball_text_styles: Vec<Vec<(&str, &str)>> = vec!(
-            vec!(("▄", "▀"), ("▗▖", "▝▘"), ("▄", "▀")),
-            vec!(("▐▌", ""), ("█", ""), ("▐▌", "")),
-            vec!(("▄", "▀"), ("▗▖", "▝▘"), ("▄", "▀"),
-        );
-            
-        let denormalized_x = self.ball.0 * 100.0;
-        let mut x_coord = 0;
+        let title = Title::from(format!("({}) - ({})", self.score.0, self.score.1).bold());
+        let description = Title::from(Line::from(vec![
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
 
-        if denormalized_x - denormalized_x.floor() > 0.33 && denormalized_x - denormalized_x.floor() < 0.66{
-            x_coord = 1;
-        }else if denormalized_x - denormalized_x.floor() > 0.66{
-            x_coord = 2;
-        }else{
-            x_coord = 0;
-        }
+        Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                description
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK)
+            .white()
+            .render(area, buf);
 
-
-        let denormalized_y = self.ball.1 * Frame::Height as f32;
-        let mut y_coord = 0;
-
-        if denormalized_y - denormalized_y.floor() > 0.33 && denormalized_y - denormalized_y.floor() < 0.66{
-            y_coord = 1;
-        }else if denormalized_y - denormalized_y.floor() > 0.66{
-            y_coord = 2;
-        }else{
-            y_coord = 0;
-        }
-
-        let ball_text = ball_text_styles[y_coord][x_coord];
-        buf.set_string(area.x + (self.ball.0 * area.width as f32) as u16 + (x_coord - 1) , area.y + (self.ball.1 * area.height as f32) as u16 - (y_coord - 1) , ball_text, Style::default());
+        Game::draw_ball(self, area, buf);
+        
     }
-
 
 }
 
